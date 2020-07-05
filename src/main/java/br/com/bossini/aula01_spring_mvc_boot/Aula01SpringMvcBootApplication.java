3080fe0516ca2e4cf6bc2f92ae3f671ee88d1565
Izabella Chianca Simonetti <izabellayeh@gmail.com>
2020-07-05 18:00:43 -0300

    Atividade semana 5

---
+++ src/main/java/br/com/bossini/aula01_spring_mvc_boot/Aula01SpringMvcBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.bossini.aula01_spring_mvc_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula01SpringMvcBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula01SpringMvcBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bossini/aula01_spring_mvc_boot/Aula01SpringMvcBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bossini.aula01_spring_mvc_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula01SpringMvcBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
